0e6d7af36a6bf9a6a05e9dc90285bc135236d6fd
Akhil Gupta <akhilgupta01@gmail.com>
2021-08-31 13:17:06 +0530

    added a cloud run container

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -1,6 +1,15 @@
+resource "google_cloud_run_service_iam_binding" "binding" {
+  location = google_cloud_run_service.crs-hello-world.location
+  project = google_cloud_run_service.crs-hello-world.project
+  service = google_cloud_run_service.crs-hello-world.name
+  role = "roles/run.admin"
+  members = ["group:devops-sa@cloud-run-trials.iam.gserviceaccount.com"]
+}
+
 resource "google_cloud_run_service" "crs-hello-world" {
   name = "crs-hello-world"
   location = var.region
+  project = var.project_id
 
   template {
     spec {
@@ -15,3 +24,4 @@ resource "google_cloud_run_service" "crs-hello-world" {
     latest_revision = true
   }
 }
+
