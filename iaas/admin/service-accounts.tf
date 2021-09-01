56fbac740b1ed3e8daa2eb24d78a6c5e6e70251b
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-01 21:58:59 +0530

    added sa user role to appplication_sa

--- iaas/admin/service-accounts.tf
+++ iaas/admin/service-accounts.tf
@@ -1,4 +1,9 @@
-resource "google_service_account" "application_sa"{
+resource "google_service_account" "application_sa" {
   account_id = "application-sa"
   display_name = "Service account used by application during runtime"
+}
+
+resource "google_service_account_iam_binding" "application_sa_user" {
+  service_account_id = google_service_account.application_sa.account_id
+  role = "roles/iam.serviceAccountUser"
 }
