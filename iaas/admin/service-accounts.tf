9ad7e646011e8a868c0de1ba5099dc6912636093
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-01 19:27:02 +0530

    corrected project name

--- iaas/admin/service-accounts.tf
+++ iaas/admin/service-accounts.tf
@@ -1,4 +1,4 @@
 resource "google_service_account" "application_sa"{
   account_id = "application-sa"
-  display_name = "Service Account used by application during runtime"
+  display_name = "Service account used by application during runtime"
 }
