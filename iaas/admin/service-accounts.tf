e88d79ef6637885671040347615e3264e8b9d982
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-03 10:24:40 +0530

    removed display name of servie account

--- iaas/admin/service-accounts.tf
+++ iaas/admin/service-accounts.tf
@@ -1,6 +1,6 @@
 resource "google_service_account" "application_sa" {
   account_id = "application-sa"
-  display_name = "Service account used by application during runtime"
+  description = "Service account used by application during runtime"
 }
 
 resource "google_service_account_iam_member" "application_sa_user" {
