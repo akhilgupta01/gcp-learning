d0f23a34be5afb06fd8d310324bb40d6ccc9c920
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-03 11:14:54 +0530

    all roles captured as csv

--- iaas/admin/service-accounts.tf
+++ iaas/admin/service-accounts.tf
@@ -1,6 +1,19 @@
+#app deployer service account
+resource "google_service_account" "app_deployer" {
+  #name =
+  account_id = "app-deployer"
+  description = "Service account used to deploy applications in this project"
+}
+resource "google_service_account_iam_member" "app_deployer" {
+  service_account_id = google_service_account.app_deployer.name
+  role = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${google_service_account.application_sa.email}"
+}
+
+
 resource "google_service_account" "application_sa" {
-  name = "application-sa"
-  account_id = "application-sa2"
+  #name =
+  account_id = "application-sa"
   description = "Service account used by application during runtime"
 }
 
