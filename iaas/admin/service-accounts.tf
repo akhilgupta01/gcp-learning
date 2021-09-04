3ba649853e7c2f9ff4f76c8648b06710462e07b3
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-04 11:39:44 +0530

    cloud run deployment

--- iaas/admin/service-accounts.tf
+++ iaas/admin/service-accounts.tf
@@ -27,4 +27,9 @@ resource "google_service_account" "application_sa" {
   account_id = "application-sa"
   description = "Service account used by application during runtime"
 }
+resource "google_service_account_iam_member" "application_sa_service_account_user" {
+  service_account_id = google_service_account.application_sa.name
+  role = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${google_service_account.application_sa.email}"
+}
 
