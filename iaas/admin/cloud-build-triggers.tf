a71da840aee04dc074b6b9290c2a8a4680f9692c
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-04 10:58:44 +0530

    build trigger added

---
+++ iaas/admin/cloud-build-triggers.tf
@@ -0,0 +1,14 @@
+resource "google_cloudbuild_trigger" "hello-world-app-deploy" {
+  name = "hello-world-app-deploy"
+  description = "Deploys Hello World application"
+  github {
+    owner = "akhilgupta01"
+    name = "gcp-learning"
+    push{
+      branch = "master"
+    }
+  }
+
+  filename = "iaas/dev/cloudbuild.yaml"
+  included_files = "iaas/dev/**"
+}
