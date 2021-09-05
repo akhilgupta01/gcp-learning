11aee1080916d8cb3612a9349ef3976cf771231c
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-05 23:54:01 +0530

    added spring job

--- hello-world-app/gcp/cloudrun.tf
+++ hello-world-app/gcp/cloudrun.tf
@@ -6,7 +6,7 @@ resource "google_cloud_run_service" "crs-hello-world" {
   template {
     spec {
       containers {
-        image = "gcr.io/ag-learn-gcp/hello-world-app:1.5-SNAPSHOT"
+        image = "gcr.io/ag-learn-gcp/hello-world-app:1.6-SNAPSHOT"
       }
       service_account_name = "application-sa@${var.project}.iam.gserviceaccount.com"
     }
