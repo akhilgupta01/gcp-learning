d225939c152a2fb7c014471d73b2402f2aa3f9a0
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-05 21:23:15 +0530

    added capability to read from Google Cloud Storage

--- hello-world-app/src/main/java/com/guptakh/gcp/JobsController.java
+++ hello-world-app/src/main/java/com/guptakh/gcp/JobsController.java
@@ -1,14 +1,22 @@
 package com.guptakh.gcp;
 
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class JobsController {
+
+    @Value("gs://${gcs-resource-test-bucket}/all_roles.csv")
+    private Resource gsFile;
 
     @GetMapping("/jobs/{jobName}")
     public String runJob(@PathVariable String jobName){
@@ -25,4 +33,18 @@ public class JobsController {
         }
         return String.valueOf(primeNumbers);
     }
+
+    @GetMapping("/jobs/storage")
+    @SneakyThrows
+    public String readStorage(){
+        FileReader fileReader = new FileReader(this.gsFile.getFile());
+        BufferedReader br=new BufferedReader(fileReader);
+        String line;
+        int count=0;
+        while((line=br.readLine())!=null) {
+            count++;
+        }
+        return String.valueOf(count);
+    }
+
 }
