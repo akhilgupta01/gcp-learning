fe06168cd22a1b0a82687f26fa1d85bdd387366c
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-05 23:45:21 +0530

    added spring job

--- hello-world-app/src/main/java/org/example/tests/spring/Application.java
+++ hello-world-app/src/main/java/org/example/tests/spring/Application.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.configuration.support.DefaultJobLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
@@ -23,6 +24,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
 	public AutomaticJobRegistrar registrar(List<ApplicationContextFactory> contextFactories, JobRegistry jobRegistry){
 		AutomaticJobRegistrar registrar = new AutomaticJobRegistrar();
 		registrar.setJobLoader(new DefaultJobLoader(jobRegistry));
