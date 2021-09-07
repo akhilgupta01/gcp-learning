354d4c8983c93011dfda2156cc62c96090f031b1
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-07 08:53:58 +0530

    added spring job

--- hello-world-app/src/main/java/org/example/tests/spring/writer/GCSItemWriter.java
+++ hello-world-app/src/main/java/org/example/tests/spring/writer/GCSItemWriter.java
@@ -1,26 +1,40 @@
 package org.example.tests.spring.writer;
 
-import org.springframework.batch.item.ItemWriter;
+import lombok.SneakyThrows;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.support.AbstractItemStreamItemWriter;
 import org.springframework.cloud.gcp.storage.GoogleStorageResource;
 import org.springframework.core.io.WritableResource;
 
 import java.io.OutputStream;
 import java.util.List;
 
-public class GCSItemWriter<T> implements ItemWriter<T> {
+public class GCSItemWriter<T> extends AbstractItemStreamItemWriter<T> {
 
     private WritableResource resource;
+
+    private OutputStream outputStream;
 
     public GCSItemWriter(GoogleStorageResource resource) {
         this.resource = resource;
     }
 
     @Override
+    @SneakyThrows
+    public void close() {
+        outputStream.close();
+    }
+
+    @Override
+    @SneakyThrows
+    public void open(ExecutionContext executionContext) {
+        outputStream = resource.getOutputStream();
+    }
+
+    @Override
     public void write(List<? extends T> items) throws Exception {
-        try (OutputStream outputStream = resource.getOutputStream()) {
-            for (T item : items) {
-                outputStream.write(String.valueOf(item).getBytes());
-            }
+        for(T item: items){
+            outputStream.write(String.valueOf(item).getBytes());
         }
     }
 }
