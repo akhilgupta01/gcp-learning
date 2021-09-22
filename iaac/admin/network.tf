b1f23547a00a97c0dc681810d009a34305a65bb3
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-22 15:14:51 +0530

    added composer

--- iaac/admin/network.tf
+++ iaac/admin/network.tf
@@ -1,13 +1,12 @@
 resource "google_compute_network" "vpc_network" {
-  count = var.create_network ? 1:0
   name = "learn-gcp-network"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnetwork"{
   project = var.project
-  name = "${google_compute_network.vpc_network[0].name}-${var.region}"
-  network = google_compute_network.vpc_network[0].name
+  name = "${google_compute_network.vpc_network.name}-${var.region}"
+  network = google_compute_network.vpc_network.name
   region = var.region
   ip_cidr_range = "10.154.0.0/20"
   private_ip_google_access = true
