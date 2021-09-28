09a07fc93b6477a6501985affc6f4005b4f6e0d2
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-28 11:51:18 +0530

    added airflow 2

--- iaac/admin/main.tf
+++ iaac/admin/main.tf
@@ -7,10 +7,21 @@ terraform {
     google = {
       source = "hashicorp/google"
     }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "3.86.0"
+    }
   }
 }
 
 provider "google" {
+  version = "3.86.0"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+provider "google-beta" {
   version = "3.86.0"
   project = var.project
   region  = var.region
