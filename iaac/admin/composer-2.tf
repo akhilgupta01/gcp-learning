57d5f9bd3b8c042f30c6b6a8c89c7f8df2a78c5d
Akhil Gupta <akhilgupta01@gmail.com>
2021-09-28 12:14:46 +0530

    added airflow 2

--- iaac/admin/composer-2.tf
+++ iaac/admin/composer-2.tf
@@ -1,22 +1,28 @@
-resource "google_composer_environment" "composer_env2" {
-  provider = google-beta
-  count    = var.enable_composer ? 1 : 0
-  name     = "composer2-env"
-  region   = var.region
-  project  = var.project
+resource "google_composer_environment" "composer_env2"{
+  count = var.enable_composer ? 1:0
+  name = "composer-env2"
+  region = var.region
+  project = var.project
   config {
+    node_count = 3
     node_config {
-      network         = google_compute_network.vpc_network.id
-      subnetwork      = google_compute_subnetwork.subnetwork.id
+      zone = var.zone
+      machine_type = "n1-standard-1"
+      network = google_compute_network.vpc_network.id
+      subnetwork = google_compute_subnetwork.subnetwork.id
       service_account = google_service_account.composer_sa.name
-      zone            = var.zone
+      ip_allocation_policy {
+        use_ip_aliases = true
+      }
     }
     private_environment_config {
-      enable_private_endpoint   = true
+      enable_private_endpoint = true
       cloud_sql_ipv4_cidr_block = "10.0.0.0/12"
-      master_ipv4_cidr_block    = "172.16.10.0/23"
+      master_ipv4_cidr_block = "172.16.10.0/23"
+      web_server_ipv4_cidr_block = "172.31.250.0/24"
     }
     software_config {
+      python_version = "3"
       image_version = "composer-1.17.1-airflow-2.1.2"
       pypi_packages = {
         google-api-python-client = ""
