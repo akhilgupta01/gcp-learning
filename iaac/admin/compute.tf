19a72a39368d64bda1899f1e7f7a308b6a2c5779
Akhil Gupta <akhilgupta01@gmail.com>
2021-10-14 09:53:38 +0530

    updated terraform version

--- iaac/admin/compute.tf
+++ iaac/admin/compute.tf
@@ -1,6 +1,7 @@
 resource "google_compute_instance" "vm_instance" {
   name = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = "n2-standard-2"
+
   zone = var.zone
   tags = [
     "web",
