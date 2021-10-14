0da1b4c55d3e9c88f6c09678b1757686f85dd336
Akhil Gupta <akhilgupta01@gmail.com>
2021-10-14 11:51:48 +0530

    updated project id

--- iaac/admin/compute.tf
+++ iaac/admin/compute.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "vm_instance" {
   name = "terraform-instance"
-  machine_type = "f1-micro"
-  #machine_type = "n2-standard-2"
+  #machine_type = "f1-micro"
+  machine_type = "n2-standard-2"
   allow_stopping_for_update = true
 
   zone = var.zone
