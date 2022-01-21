d8891ec8773ce86cc66adda7ec5d08e1dfb91b60
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-21 12:40:59 +0530

    added file for reference

--- iaac/admin/bq_dataset.tf
+++ iaac/admin/bq_dataset.tf
@@ -1,26 +1,18 @@
 resource "google_bigquery_dataset" "dataset" {
-  dataset_id                  = "transactions_dataset"
-  friendly_name               = "transactions"
-  description                 = "This is a test dataset for transactions"
+  dataset_id    = "transactions_dataset"
+  friendly_name = "transactions"
+  description   = "This is a test dataset for transactions"
 }
 
 resource "google_bigquery_table" "default" {
   dataset_id = google_bigquery_dataset.dataset.dataset_id
-  table_id   = "system-x"
+  table_id   = "passengers"
 
-  time_partitioning {
-    type = "DAY"
+  external_data_configuration {
+    autodetect    = true
+    source_format = "CSV"
+    source_uris = [
+      "gs://ag-trial-project-1_work_dir/incoming/passengers.csv",
+    ]
   }
-
-  labels = {
-    env = "default"
-  }
-
-  schema = <<EOF
-[
-  {"name": "tx_id", "type": "STRING", "mode": "Required","description": "ID of the transaction"},
-  {"name": "tx_date", "type": "DATETIME", "mode": "Required","description": "Datetime when the transaction was created"}
-]
-EOF
-
 }
