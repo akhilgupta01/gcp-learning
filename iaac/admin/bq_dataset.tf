ae1c48e693dc8feaafe11f70d32e7af3644e060c
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-21 10:43:43 +0530

    added table

--- iaac/admin/bq_dataset.tf
+++ iaac/admin/bq_dataset.tf
@@ -3,3 +3,24 @@ resource "google_bigquery_dataset" "dataset" {
   friendly_name               = "transactions"
   description                 = "This is a test dataset for transactions"
 }
+
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = "system-x"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+  {"name": "tx_id", "type": "STRING", "mode": "Required","description": "ID of the transaction"},
+  {"name": "tx_date", "type": "DATETIME", "mode": "Required","description": "Datetime when the transaction was created"}
+]
+EOF
+
+}
