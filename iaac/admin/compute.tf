935b83b2bfc51515c1184de1d5912d7e835c9693
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-21 10:27:22 +0530

    removed compute instance creation

--- iaac/admin/compute.tf
+++ iaac/admin/compute.tf
@@ -1,20 +1,20 @@
-resource "google_compute_instance" "vm_instance" {
-  name = "terraform-instance"
-  #machine_type = "f1-micro"
-  machine_type = "n2-standard-2"
-  allow_stopping_for_update = true
-
-  zone = var.zone
-  tags = [
-    "web",
-    "dev"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-  network_interface {
-    network = google_compute_network.vpc_network.name
-  }
-}
+#resource "google_compute_instance" "vm_instance" {
+#  name = "terraform-instance"
+#  #machine_type = "f1-micro"
+#  machine_type = "n2-standard-2"
+#  allow_stopping_for_update = true
+#
+#  zone = var.zone
+#  tags = [
+#    "web",
+#    "dev"]
+#
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-9"
+#    }
+#  }
+#  network_interface {
+#    network = google_compute_network.vpc_network.name
+#  }
+#}
