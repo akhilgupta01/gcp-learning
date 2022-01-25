8f71c5e3e1d56fe2c2b027568b8230d7b40fc35c
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-25 09:58:43 +0530

    start/stop compute instance using cloud functions

--- cloud-infra/project1/src/main.py
+++ cloud-infra/project1/src/main.py
@@ -1,6 +1,6 @@
 import google.cloud.compute_v1 as compute_v1
 
-def startInstancePubSub(request):
+def startInstancePubSub(request, event):
     instance_client = compute_v1.InstancesClient()
     instance_list = instance_client.list(project='ag-trial-project-1', zone='us-central1-a')
     for instance in instance_list:
