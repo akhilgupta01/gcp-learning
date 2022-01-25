c2eab7c019e0e29e04a2d3a1746b3936bee7e006
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-25 13:55:16 +0530

    upgraded terraform module version

--- cloud-infra/project1/terraform/compute.tf
+++ cloud-infra/project1/terraform/compute.tf
@@ -1,9 +1,15 @@
 resource "google_compute_instance" "dev_instance" {
   name = "terraform-instance"
-  machine_type = "f1-micro"
-  #machine_type = "n2-standard-2"
+  machine_type = "n1-standard-1"
   allow_stopping_for_update = true
-  labels = {"env":"dev"}
+  labels = {
+    "env":"dev",
+    "container-vm": "cos-stable-93-16623-102-4"
+  }
+  metadata = {
+    "gce-container-declaration" = "spec:\n  containers:\n    - name: instance-4\n      image: 'gcr.io/cloud-marketplace/google/nginx1:1.12'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
+    "google-logging-enabled": "true"
+  }
   zone = var.zone
   tags = ["web","dev"]
 
@@ -14,8 +20,5 @@ resource "google_compute_instance" "dev_instance" {
   }
   network_interface {
     network = google_compute_network.vpc_network.name
-  }
-  metadata {
-    gce-container-declaration = "spec:\n  containers:\n    - name: instance-4\n      image: 'gcr.io/cloud-marketplace/google/nginx1:1.12'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
   }
 }
