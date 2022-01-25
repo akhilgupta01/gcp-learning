05b2d6c814da529ec6f1dfc55b24d1a263c9bec0
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-25 11:01:59 +0530

    start/stop compute instance using cloud functions

--- cloud-infra/project1/src/main.py
+++ cloud-infra/project1/src/main.py
@@ -1,25 +1,27 @@
 import google.cloud.compute_v1 as compute_v1
-import json
+import base64
 
-def manage_vm_instance(message, event):
-    print(message)
-    print(message['data'])
-    print(message.data)
-    data_json = json.loads(message.data.decode('utf-8'))
-    project_id = data_json['project_id']
-    zone = data_json['zone']
-    vm_instance = data_json['vm_instance']
-    action = data_json['action']
-    instance_client = compute_v1.InstancesClient()
-    instance_list = instance_client.list(project=project_id, zone=zone)
-    if action == 'start':
-        print(f"Received request to start compute instance {vm_instance}")
-        for instance in instance_list:
-            print(f"Starting instance - {instance.name}")
-            instance_client.start_unary(project=project_id, zone=zone, instance=instance.name)
-    elif action == 'stop':
-        print(f"Received request to stop compute instance {vm_instance}")
-        for instance in instance_list:
-            print(f"Stopping instance - {instance.name}")
-            instance_client.stop_unary(project=project_id, zone=zone, instance=instance.name)
-    return "OK"
+
+def manage_vm_instance(event, context):
+    if 'data' in event:
+        message = base64.b64decode(event['data']).decode('utf-8')
+        print(message)
+        print(message['project_id'])
+        project_id = message['project_id']
+        zone = message['zone']
+        vm_instance = message['vm_instance']
+        action = message['action']
+        instance_client = compute_v1.InstancesClient()
+        instance_list = instance_client.list(project=project_id, zone=zone)
+        if action == 'start':
+            print(f"Received request to start compute instance {vm_instance}")
+            for instance in instance_list:
+                print(f"Starting instance - {instance.name}")
+                instance_client.start_unary(project=project_id, zone=zone, instance=instance.name)
+        elif action == 'stop':
+            print(f"Received request to stop compute instance {vm_instance}")
+            for instance in instance_list:
+                print(f"Stopping instance - {instance.name}")
+                instance_client.stop_unary(project=project_id, zone=zone, instance=instance.name)
+    else:
+        print("No data found in event")
