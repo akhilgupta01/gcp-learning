4d5a58a4940f37cb4f841deed72c04ba04deb06e
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-25 11:08:08 +0530

    start/stop compute instance using cloud functions

--- cloud-infra/project1/src/main.py
+++ cloud-infra/project1/src/main.py
@@ -1,16 +1,19 @@
 import google.cloud.compute_v1 as compute_v1
 import base64
+import json
 
 
 def manage_vm_instance(event, context):
     if 'data' in event:
         message = base64.b64decode(event['data']).decode('utf-8')
         print(message)
-        print(message['project_id'])
-        project_id = message['project_id']
-        zone = message['zone']
-        vm_instance = message['vm_instance']
-        action = message['action']
+        message_json = json.loads(message)
+        print(message_json)
+        print(message_json['project_id'])
+        project_id = message_json['project_id']
+        zone = message_json['zone']
+        vm_instance = message_json['vm_instance']
+        action = message_json['action']
         instance_client = compute_v1.InstancesClient()
         instance_list = instance_client.list(project=project_id, zone=zone)
         if action == 'start':
