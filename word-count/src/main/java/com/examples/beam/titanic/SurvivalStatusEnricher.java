f129f6b9840cf2a6e7a8eff0c730383b5fb2a0b0
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-27 10:52:09 +0530

    added dataflow module

--- word-count/src/main/java/com/examples/beam/titanic/SurvivalStatusEnricher.java
+++ word-count/src/main/java/com/examples/beam/titanic/SurvivalStatusEnricher.java
@@ -1,34 +1,25 @@
 package com.examples.beam.titanic;
 
 import com.examples.beam.titanic.model.Passenger;
-import com.examples.beam.titanic.service.HelplineService;
 import org.apache.beam.sdk.transforms.DoFn;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 class SurvivalStatusEnricher extends DoFn<List<Passenger>, List<Passenger>> {
-    private HelplineService helplineService;
-
-    @Setup
-    public void init() {
-        this.helplineService = new HelplineService();
-    }
 
     @ProcessElement
     public void enrich(@Element List<Passenger> passengers, ProcessContext context) {
         List<String> passengerIds = passengers.stream()
                 .map(Passenger::getPassengerId)
                 .collect(Collectors.toList());
-        Map<String, Boolean> survivalStatusMap = helplineService.hasSurvived(passengerIds);
         context.output(passengers.stream()
                 .map(passenger -> Passenger.builder()
                         .passengerId(passenger.getPassengerId())
                         .name(passenger.getName())
                         .age(passenger.getAge())
                         .sex(passenger.getSex())
-                        .survived(survivalStatusMap.get(passenger.getPassengerId()))
+                        .survived(true)
                         .build())
                 .collect(Collectors.toList()));
     }
