6fb6f796dc08968cc499c61359cad48ca5fc4feb
Akhil Gupta <akhilgupta01@gmail.com>
2022-01-31 12:57:26 +0530

    added dataflow module

--- word-count/src/main/java/com/examples/beam/titanic/TitanicJobOptions.java
+++ word-count/src/main/java/com/examples/beam/titanic/TitanicJobOptions.java
@@ -5,8 +5,9 @@ import org.apache.beam.sdk.options.*;
 public interface TitanicJobOptions extends PipelineOptions {
     @Description("Input for the pipeline")
     @Default.String("gs://ag-trial-project-a_work_dir/incoming/passengers.csv")
+    @Validation.Required
     ValueProvider<String> getInput();
-    void setInput(ValueProvider<String> input);
+    void setInput(ValueProvider<String> value);
 
     @Description("Valid Output for the pipeline")
     @Default.String("gs://ag-trial-project-a_work_dir/outgoing/titanic_valid.csv")
