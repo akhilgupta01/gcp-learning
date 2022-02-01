39f2b774be1788c4909aae49a9478cadd9c2b2c4
Akhil Gupta <akhilgupta01@gmail.com>
2022-02-01 10:27:39 +0530

    added transaction use case

--- word-count/src/main/java/com/examples/beam/core/model/EligibilityStatus.java
+++ word-count/src/main/java/com/examples/beam/core/model/EligibilityStatus.java
@@ -3,9 +3,11 @@ package com.examples.beam.core.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class EligibilityStatus {
+public class EligibilityStatus implements Serializable {
     private boolean eligible;
     private String reason;
 }
