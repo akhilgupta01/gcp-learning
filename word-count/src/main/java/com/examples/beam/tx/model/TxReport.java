00cfa53f563219c09d8b30108fd040fc80916f81
Akhil Gupta <akhilgupta01@gmail.com>
2022-02-01 10:25:15 +0530

    added transaction use case

--- word-count/src/main/java/com/examples/beam/tx/model/TxReport.java
+++ word-count/src/main/java/com/examples/beam/tx/model/TxReport.java
@@ -3,9 +3,11 @@ package com.examples.beam.tx.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class TxReport {
+public class TxReport implements Serializable {
     private String isin;
     private Integer totalQty;
     private Integer minAmount;
