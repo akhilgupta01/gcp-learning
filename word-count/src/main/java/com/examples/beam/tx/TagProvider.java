791a8f1a963ffe80e57b7ab2bed3ded6e347920f
Akhil Gupta <akhilgupta01@gmail.com>
2022-02-01 10:36:06 +0530

    added transaction use case

--- word-count/src/main/java/com/examples/beam/tx/TagProvider.java
+++ word-count/src/main/java/com/examples/beam/tx/TagProvider.java
@@ -4,11 +4,9 @@ import com.examples.beam.tx.model.Transaction;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.beam.sdk.values.TupleTagList;
 
-import java.io.Serializable;
-
-public class TagProvider implements Serializable {
-    public static final TupleTag<Transaction> INGESTION_SUCCESS_TAG = new TupleTag<Transaction>();
-    public static final TupleTag<String> PARSE_FAILURE_TAG = new TupleTag<String>();
+public class TagProvider {
+    public static final TupleTag<Transaction> INGESTION_SUCCESS_TAG = new TupleTag<Transaction>(){};
+    public static final TupleTag<String> PARSE_FAILURE_TAG = new TupleTag<String>(){};
 
     public static final TupleTagList INGESTION_FAILURE_TAGS = TupleTagList.of(PARSE_FAILURE_TAG);
 }
